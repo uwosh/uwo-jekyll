96ca0186c6b554e8102a9d2c28feda932f81ff82
Shawn Hansen <shawn.hansen@gmail.com>
2013-10-29 16:10:07 -0500

    Update to load thumbnail of YouTube video instead of iframe. When thumbnail is clicked, video loads in place. Using code from Google+ to make this work.

--- _plugins/videophoto.rb
+++ _plugins/videophoto.rb
@@ -5,9 +5,19 @@ module Jekyll
 
     def videophoto(input)
       doc = Nokogiri::HTML(input)
-      iframe = doc.xpath('//iframe')
+      iframe = doc.xpath('//iframe/@src')
 
-      iframe[0].to_s
+      if iframe[0].to_s != ''
+        # This regex pulls youtube video ID from various styles of youtube URLs
+        # Taken from http://stackoverflow.com/questions/3452546/javascript-regex-how-to-get-youtube-video-id-from-url/4811367#4811367
+        re = /^.*(youtu.be\/|v\/|embed\/|watch\?|youtube.com\/user\/[^#]*#([^\/]*?\/)*)\??v?=?([^#\&\?]*).*/
+        yt = iframe[0].to_s
+
+        src = yt.match re
+
+        # 3rd match from regex contains the youtubeID
+        src[3]
+      end
     end
   end
 end
