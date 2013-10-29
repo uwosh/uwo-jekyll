3f39c3035aceee4068d2bfe16b24d6f168970268
Shawn Hansen <shawn.hansen@gmail.com>
2013-10-29 16:34:18 -0500

    Add htmlhint to Gruntfile. Initial configuration of html hinting.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,27 @@
 module.exports = function(grunt){
 
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json')
-    });
+  require("matchdep").filterDev("grunt-*").forEach(grunt.loadNpmTasks);
 
-    grunt.registerTask('default', []);
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    htmlhint: {
+      build: {
+        options: {
+          'tag-pair': true,
+          'tagname-lowercase': true,
+          'attr-lowercase': true,
+          'attr-value-double-quotes': true,
+          'doctype-first': true,
+          'spec-char-escape': true,
+          'id-unique': true,
+          'head-script-disabled': true,
+          'style-disabled': true
+        },
+        src: ['index.html']
+      }
+    }
+  });
+
+  grunt.registerTask('default', []);
 
 };
