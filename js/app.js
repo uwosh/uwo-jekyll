28f8738cccbc6d34d8b0d93803d09bdf272c8a51
Shawn Hansen <shawn.hansen@gmail.com>
2013-12-06 11:51:51 -0600

    Rotate clickables when clicked.

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+$(document).ready(function() {
+  $('.clickable').click(function() {
+    $('i.clickable').toggleClass('rotate180');
+  });
+});
