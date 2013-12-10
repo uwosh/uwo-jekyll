7fd19a394d5f28b627500f14698784197788d86d
Shawn Hansen <shawn.hansen@gmail.com>
2013-12-10 14:49:05 -0600

    Comment out clickable rotate code for now.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
-$(document).ready(function() {
-  $('.clickable').click(function() {
-    $('i.clickable').toggleClass('rotate180');
-  });
-});
+
+//$(document).ready(function() {
+//  $('.clickable').click(function() {
+//    $('i.clickable').toggleClass('rotate180');
+//  });
+//});
