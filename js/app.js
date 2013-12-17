462c304dedcdf838e8acc2ccf0768938a9d5e5e7
Shawn Hansen <shawn.hansen@gmail.com>
2013-12-17 16:39:42 -0600

    Add knockout. Grab json from api server.

--- js/app.js
+++ js/app.js
@@ -7,3 +7,9 @@ $(document).foundation();
 //    $('i.clickable').toggleClass('rotate180');
 //  });
 //});
+
+$.getJSON("http://feeds.uwosh.edu/api/v1/index.php/combined", function(data) {
+    // Now use this data to update your view models,
+    // and Knockout will update your UI automatically
+    console.log(data);
+});
