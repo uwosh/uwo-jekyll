1b5ff4b29a0b222bc760c2b81070efa4af0edb84
Shawn Hansen <shawn.hansen@gmail.com>
2013-12-18 15:18:35 -0600

    Update panels to live data.

--- js/angular-app.js
+++ js/angular-app.js
@@ -50,6 +50,33 @@ angular.module('eosApp')
     }).error(function() {
       // Some error occurred
     });
+  })
+  .controller('PoweringCommunityPanelCtrl', function ($scope,$http,feedService) {
+    feedService.events().success(function(data) {
+      $scope.post = data.poweringCommunity.posts[0];
+      $scope.post.category = data.poweringCommunity.category.title;
+      $scope.categoryURL = 'http://www.uwosh.edu/today/' + data.poweringCommunity.category.slug;
+      $scope.slug = data.poweringCommunity.category.slug;
+      var presubstr = data.poweringCommunity.posts[0].excerpt;
+      var strippedstr = presubstr.replace(/(<([^>]+)>)/ig,'');
+      $scope.excerpt = strippedstr;
+    }).error(function() {
+      // Some error occurred
+    });
+  })
+  .controller('UwotwPanelCtrl', function ($scope,$http,feedService) {
+    feedService.events().success(function(data) {
+      $scope.post = data.uwotw.posts[0];
+      $scope.post.category = data.uwotw.category.title;
+      $scope.categoryURL = 'http://www.uwosh.edu/today/' + data.uwotw.category.slug;
+      $scope.slug = data.uwotw.category.slug;
+      $scope.youtube_id = data.uwotw.posts[0].custom_fields.youtube_id
+      var presubstr = data.uwotw.posts[0].excerpt;
+      var strippedstr = presubstr.replace(/(<([^>]+)>)/ig,'');
+      $scope.excerpt = strippedstr;
+    }).error(function() {
+      // Some error occurred
+    });
   });
 
 // FILTERS
