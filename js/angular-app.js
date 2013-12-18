0258a7eadbd5bf027909a8fa55312a9edbc6d1de
Shawn Hansen <shawn.hansen@gmail.com>
2013-12-18 13:49:15 -0600

    Update Research News panel to use live data.

--- js/angular-app.js
+++ js/angular-app.js
@@ -37,6 +37,19 @@ angular.module('eosApp')
     }).error(function() {
       // Some error occurred
     });
+  })
+  .controller('ResearchNewsPanelCtrl', function ($scope,$http,feedService) {
+    feedService.events().success(function(data) {
+      $scope.post = data.researchNews.posts[0];
+      $scope.post.category = data.researchNews.category.title;
+      $scope.categoryURL = 'http://www.uwosh.edu/today/' + data.researchNews.category.slug;
+      $scope.slug = data.researchNews.category.slug;
+      var presubstr = data.researchNews.posts[0].excerpt;
+      var strippedstr = presubstr.replace(/(<([^>]+)>)/ig,'');
+      $scope.excerpt = strippedstr;
+    }).error(function() {
+      // Some error occurred
+    });
   });
 
 // FILTERS
