fe5bb5c8dd7e849aa7f99736ea50fc3a0a05121a
Shawn Hansen <shawn.hansen@gmail.com>
2013-12-18 15:25:50 -0600

    Add Admissions controller.

--- js/angular-app.js
+++ js/angular-app.js
@@ -70,8 +70,20 @@ angular.module('eosApp')
       $scope.post.category = data.uwotw.category.title;
       $scope.categoryURL = 'http://www.uwosh.edu/today/' + data.uwotw.category.slug;
       $scope.slug = data.uwotw.category.slug;
-      $scope.youtube_id = data.uwotw.posts[0].custom_fields.youtube_id
       var presubstr = data.uwotw.posts[0].excerpt;
+      var strippedstr = presubstr.replace(/(<([^>]+)>)/ig,'');
+      $scope.excerpt = strippedstr;
+    }).error(function() {
+      // Some error occurred
+    });
+  })
+  .controller('AdmissionsPanelCtrl', function ($scope,$http,feedService) {
+    feedService.events().success(function(data) {
+      $scope.post = data.admissions.posts[0];
+      $scope.post.category = data.admissions.category.title;
+      $scope.categoryURL = 'http://www.uwosh.edu/today/' + data.admissions.category.slug;
+      $scope.slug = data.admissions.category.slug;
+      var presubstr = data.admissions.posts[0].excerpt;
       var strippedstr = presubstr.replace(/(<([^>]+)>)/ig,'');
       $scope.excerpt = strippedstr;
     }).error(function() {
