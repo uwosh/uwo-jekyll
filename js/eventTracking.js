5398c36c3e8602f2ed3a93ae6c51c5a64327ba1c
Shawn Hansen <shawn.hansen@gmail.com>
2014-01-31 13:57:09 -0600

    Add event tracking for home page cards.

--- js/eventTracking.js
+++ js/eventTracking.js
@@ -1,4 +1,4 @@
-// University Navigation
+// UNIVERSITY NAVIGATION
 $('#aboutToggle').on('mouseenter', function() {
   ga('send', 'event', 'University Navigation', 'hover', 'About');
 });
@@ -61,3 +61,75 @@ $('#titanServicesToggle').on('mouseenter', function() {
 $('#titanServicesToggle').on('click', function() {
   ga('send', 'event', 'University Navigation', 'click', 'Titan Services');
 });
+// END UNIVERSITY NAVIGATION
+
+// HOME PAGE CARDS
+$('#campusNewsPanel').on('mouseenter', function() {
+  ga('send', 'event', 'Home Page Cards', 'hover', 'Campus News');
+});
+$('#campusNewsFullStory').on('click', function() {
+  ga('send', 'event', 'Home Page Cards', 'click', 'Campus News');
+});
+
+$('#researchNewsPanel').on('mouseenter', function() {
+  ga('send', 'event', 'Home Page Cards', 'hover', 'Research News');
+});
+$('#researchNewsFullStory').on('click', function() {
+  ga('send', 'event', 'Home Page Cards', 'click', 'Research News');
+});
+
+$('#poweringCommunityPanel').on('mouseenter', function() {
+  ga('send', 'event', 'Home Page Cards', 'hover', 'Powering Community');
+});
+$('#poweringCommunityFullStory').on('click', function() {
+  ga('send', 'event', 'Home Page Cards', 'click', 'Powering Community');
+});
+
+$('#uwotwPanel').on('mouseenter', function() {
+  ga('send', 'event', 'Home Page Cards', 'hover', 'UW Oshkosh This Week');
+});
+$('#uwotwFullStory').on('click', function() {
+  ga('send', 'event', 'Home Page Cards', 'click', 'UW Oshkosh This Week');
+});
+$('#uwotwVideo').on('click', function() {
+  ga('send', 'event', 'Home Page Cards', 'play', 'UW Oshkosh This Week');
+});
+
+$('#studyAtUwoPanel').on('mouseenter', function() {
+  ga('send', 'event', 'Home Page Cards', 'hover', 'Study at UW Oshkosh');
+});
+$('#studyAtUwoFullStory').on('click', function() {
+  ga('send', 'event', 'Home Page Cards', 'click', 'Study at UW Oshkosh');
+});
+
+$('#iAmATitanPanel').on('mouseenter', function() {
+  ga('send', 'event', 'Home Page Cards', 'hover', 'I Am A Titan');
+});
+$('#iAmATitanFullStory').on('click', function() {
+  ga('send', 'event', 'Home Page Cards', 'click', 'I Am A Titan');
+});
+
+$('#fromTheChancellorPanel').on('mouseenter', function() {
+  ga('send', 'event', 'Home Page Cards', 'hover', 'From the Chancellor');
+});
+$('#fromTheChancellorFullStory').on('click', function() {
+  ga('send', 'event', 'Home Page Cards', 'click', 'From the Chancellor');
+});
+
+$('#meetUwOshkoshPanel').on('mouseenter', function() {
+  ga('send', 'event', 'Home Page Cards', 'hover', 'Meet UW Oshkosh');
+});
+$('#meetUwOshkoshFullStory').on('click', function() {
+  ga('send', 'event', 'Home Page Cards', 'click', 'Meet UW Oshkosh');
+});
+
+$('#facultyFivePanel').on('mouseenter', function() {
+  ga('send', 'event', 'Home Page Cards', 'hover', 'Faculty Five');
+});
+$('#facultyFiveFullStory').on('click', function() {
+  ga('send', 'event', 'Home Page Cards', 'click', 'Faculty Five');
+});
+$('#facultyFiveVideo').on('click', function() {
+  ga('send', 'event', 'Home Page Cards', 'play', 'Faculty Five');
+});
+// END HOME PAGE CARDS
