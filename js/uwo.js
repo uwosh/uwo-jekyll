1444915bb92a77c11be889dea412a50cd0ae6e0e
Shawn Hansen <shawn.hansen@gmail.com>
2014-02-19 16:16:58 -0600

    Turn hashtags and mentions to links in social feed.

--- js/uwo.js
+++ js/uwo.js
@@ -9369,7 +9369,27 @@ angular.module('eosApp')
       }
       return input;
     };
-  });
+  })
+  .filter('tweetLinky',['$filter',
+    function($filter) {
+      return function(text, target) {
+        if (!text) return text;
+
+        var replacedText = $filter('linky')(text, target);
+        var targetAttr = "";
+        if (angular.isDefined(target)) {
+            targetAttr = ' target="' + target + '"';
+        }
+        // replace #hashtags and send them to twitter
+        var replacePattern1 = /(^|\s)#(\w*[a-zA-Z_]+\w*)/gim;
+        replacedText = text.replace(replacePattern1, '$1<a href="https://twitter.com/search?q=%23$2"' + targetAttr + '>#$2</a>');
+        // replace @mentions but keep them to our site
+        var replacePattern2 = /(^|\s)\@(\w*[a-zA-Z_]+\w*)/gim;
+        replacedText = replacedText.replace(replacePattern2, '$1<a href="https://twitter.com/$2"' + targetAttr + '>@$2</a>');
+        return replacedText;
+      };
+    }
+  ]);
 
 /*
  * Foundation Responsive Library
