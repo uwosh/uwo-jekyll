8ca84b3e496560e3b3c7c9dba8ddd3abcd341e75
Shawn Hansen <shawn.hansen@gmail.com>
2014-06-18 09:17:54 -0500

    Change API source to UWO instead of Madison.

--- js/angular-app.js
+++ js/angular-app.js
@@ -7,7 +7,7 @@ angular.module('eosApp')
     var doRequest = function() {
       return $http({
         method: 'GET',
-        url: 'http://feeds.uwosh.edu/api/v1/index.php/uwo-api',
+        url: 'http://feeds2.uwosh.edu/api/v2/index.php/uwo-api',
         cache: true
       });
     };
