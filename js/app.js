75216ed480b54fef2ec698040312cb73808f4127
kempenk <kempenk@uwosh.edu>
2015-07-15 07:50:39 -0500

    updated banner image order

--- js/app.js
+++ js/app.js
@@ -323,6 +323,10 @@ function fetchUwoApi() {
     success: function( data )
     {
       var pillars = {
+      responsiveness: {
+        feedEntry: data.pillarResponsiveness.posts[0],
+        pillarClass: 'pillar-responsiveness'
+      },
         sustainability: {
           feedEntry: data.pillarSustainability.posts[0],
           pillarClass: 'pillar-sustainability'
@@ -338,10 +342,6 @@ function fetchUwoApi() {
         leadership: {
           feedEntry: data.pillarLeadership.posts[0],
           pillarClass: 'pillar-leadership'
-        },
-        responsiveness: {
-          feedEntry: data.pillarResponsiveness.posts[0],
-          pillarClass: 'pillar-responsiveness'
         }
       };
 
